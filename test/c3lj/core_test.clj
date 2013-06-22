410f87a84e6ef87916c4922b46762ec31e3a5150
David Conner <dconner.pro@gmail.com>
2013-06-22 07:28:42 -0600

    core_test.clj: added test for converting interest messages

--- test/c3lj/core_test.clj
+++ test/c3lj/core_test.clj
@@ -5,3 +5,14 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest serializer-interest
+  (testing "Serializer - ccnb - interest message"
+    (is (=
+      (c3lj.core/ser-intmsg 16r01d2f2faa55041524300fa9d000102000000) (
+"<Interest>
+  <Name>
+    <Component ccnbencoding="text">PARC</Component>
+    <Component ccnbencoding="base64Binary">AAEC</Component>
+  </Name>
+</Interest>")))))
